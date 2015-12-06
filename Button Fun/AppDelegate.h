7a4ece471da19c70afc5b13ab4419e6ad277fe44
Melissa Netto <Kevin@stephens-macbook-air.local>
2015-12-06 17:27:40 -0500

    Initial Commit

---
+++ Button Fun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Button Fun
+//
+//  Created by Kevin on 12/6/15.
+//  Copyright © 2015 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Button Fun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Button Fun
+//
+//  Created by Kevin on 12/6/15.
+//  Copyright © 2015 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
